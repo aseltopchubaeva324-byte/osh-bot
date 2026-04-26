affab3d5560a789b8fcb61e6fdae3f00d1196187
aseltopchubaeva324-byte <aseltopchubaeva324@gmail.com>
2026-04-26 22:43:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -192,3 +192,16 @@ app.add_handler(MessageHandler(filters.ALL, handle))
 
 print("🔥 МЭРИЯ БОТ ИШТЕП ЖАТАТ")
 app.run_polling()
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    Thread(target=run).start()
